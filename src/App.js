8eade7046d3f70a83f524eccfb3b60a53f2531a6
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-07 17:17:10 +0100

    Connected spotify API and has authorisation checks to redirect user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      window.location.hash = "loggedin";
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {token ? (
+        <div>
+          <h1>Hello</h1>
+          <h2>You are logged in</h2>
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
