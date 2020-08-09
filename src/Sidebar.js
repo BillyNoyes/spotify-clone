b0fe007e1383d5ef6b6195d37b790515993e5c91
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-09 20:48:02 +0100

    Created component for the sidebarOptions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify Logo"
+      />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={HomeIcon} title="Search" />
+      <SidebarOption Icon={HomeIcon} title="Your Library" />
     </div>
   );
 }
